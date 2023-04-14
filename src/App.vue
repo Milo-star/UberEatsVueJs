89c310d46b00951e1badce28404048bf5df5841b
Milo Bonnet <milovtt@gmail.com>
2023-04-14 13:54:34 +0200

    Add files & folders

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Home/>
+  </div>
+</template>
+
+<script>
+import Home from './page/HomePage.vue';
+    export default {
+      name: 'App',
+      components: {
+        Home,
+      }
+    }
+</script>
+
+<style>
+@import url('./styles/reset.css');
+@import url('https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap');
+#app {
+  font-family: 'Roboto', sans-serif;
+  padding: 1rem;
+}
+</style>
---
+++ src/page/HomePage.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="home">
+        <RestaurantRow v-for="(data, i) in data_restaurant" :key="i" :three_restaurant="data"/>
+    </div>
+</template>
+
+<script>
+    import RestaurantRow from '@/components/RestaurantRow.vue';
+    import bdd from '@/bdd';
+    import { onMounted, ref } from 'vue';
+
+    export default {
+        name: 'HomePage',
+        components: {
+            RestaurantRow,
+        },
+        setup(){
+            class Restaurant {
+                constructor (name, note, image, drive_time) {
+                    this.name = name;
+                    this.note = note;
+                    this.image = image;
+                    this.drive_time = drive_time;
+                }
+            }
+
+            let data_restaurant = ref([]);
+
+            const makeDataRestaurant = () => {
+                let three_restaurant = [];
+
+                for (const restaurant of bdd) {
+                    const new_restaurant = new Restaurant(restaurant.name, restaurant.note, restaurant.image, restaurant.drive_time);
+
+                    if (three_restaurant.length === 2){
+                        three_restaurant.push(new_restaurant);
+                        data_restaurant.value.push(three_restaurant);
+                        three_restaurant = [];
+                    }else{
+                        three_restaurant.push(new_restaurant);
+                    }
+                }
+            } 
+            onMounted(makeDataRestaurant);
+
+            return {
+                data_restaurant,
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
