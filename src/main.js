23d9a69d491629737fbb0638a81d69c06d5d86b0
Milo Bonnet <milovtt@gmail.com>
2023-04-18 14:40:02 +0200

    Init Vue Router

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomePage from './page/HomePage.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    { path: '/', component: HomePage },
+]
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes,
+})
+
+const VueApp = createApp(App)
+
+VueApp.use(router)
+
+.mount('#app')
