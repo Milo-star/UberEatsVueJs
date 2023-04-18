8e4ab9fd047239415489e11e329e5b98f39b1039
Milo Bonnet <milovtt@gmail.com>
2023-04-18 16:04:58 +0200

    Url custom whit slug

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomePage from './page/HomePage.vue'
+import Restaurant from './page/Restaurant.vue'
 
 const routes = [
     { path: '/', component: HomePage },
+    { name: 'Restaurant', path: '/restaurant/:name', component: Restaurant}
 ]
 
 const router = createRouter({
